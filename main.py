a86893c26e22c9198692d762410277bd6de946f3
nutubianca <bienutu@gmail.com>
2021-10-20 18:57:15 +0300

    am rezolvat 3

--- main.py
+++ main.py
@@ -17,18 +17,38 @@ def test_nr_negative_nenule():
     assert nr_negative_nenule([-1,-2,-3]) == [-1,-2,-3]
 
 
+def nr_minim_cifra_data(l,cifra):
+    nr_minim=None
+    for x in l:
+        if x%10==cifra or -x%10==cifra:
+            if nr_minim==None:
+                nr_minim=x
+            else:
+                if x<nr_minim:
+                    nr_minim=x
+    return nr_minim
+
+
+def test_nr_minim_cifra_data():
+    assert nr_minim_cifra_data([-2,3,-3,2], 2) == -2
+    assert nr_minim_cifra_data([3,13,43], 3) == 3
+    assert nr_minim_cifra_data([2,4,5], 1) == None
+
 
 def print_menu():
     print("1. Citire lista.")
-    print("2. ")
-    print("3. ")
-    print("4. ")
-    print("5. ")
+    print("2. Afișarea tuturor numerelor negative nenule din listă.")
+    print("3. Afișarea celui mai mic număr care are ultima cifră egală cu o cifră citită de la tastatură.")
+    print("4. Afișarea tuturor numerelor din listă care sunt superprime.")
+    print("5. Afișarea listei obținute din lista inițială în care numerele pozitive și nenule au fost înlocuite cu"
+          "CMMDC-ul lor și numerele negative au cifrele în ordine inversă.")
     print("a. Afisare lista.")
     print("x. Iesire.")
 
+
 def main():
     test_nr_negative_nenule()
+    test_nr_minim_cifra_data()
     while True:
         print_menu()
         optiune=input("Dati optiunea:")
@@ -39,6 +59,9 @@ def main():
             print(lista)
         elif optiune == "2":
             print(nr_negative_nenule(lista))
+        elif optiune == "3":
+            cifra=int(input("Dati cifra:"))
+            print(nr_minim_cifra_data(lista,cifra))
         elif optiune == "x":
             print("Meniul se va inchide.")
             break
